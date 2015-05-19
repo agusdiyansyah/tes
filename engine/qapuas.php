94e40dfcbbe95a44625816814de910009fe8e3ff
agusdiyansyah <kreasiin@gmail.com>
2015-05-19 09:03:05 +0700

    add autoload

--- engine/qapuas.php
+++ engine/qapuas.php
@@ -1,10 +1,38 @@
 <?php 
-
-	define('APP', '../app/');
-	define('CORE', 'core/');
-	define('LIB', 'library/');
-	define('FUN', 'functions/');
+	define("APP", "../app/");
+	define("AST", "gudang/");
+	define("CORE", "core/");
+	define("LIB", "library/");
+	define("FUN", "function/");
 	
-	require_once CORE . 'init.php';
+	/**
+	AUTOLOAD
+	*/
+
+	/*
+	MUST BE LOADED
+	---------------------------
+	app 	: routing class
+	loader 	: file loader class to load module, model, view, and controller
+
+	OPTIONAL
+	---------------------------
+	model 	: database class
+	*/
+	$conf["CORE"] = ["app","loader","model"];
+
+	// Load class
+	$conf["LIB"]  = [""];
+
+	// Load function
+	$conf["FUN"]  = [""];
+
+	foreach ($conf as $key => $value) {
+		for ($i=0; $i < count($conf[$key]); $i++) { 
+			if (file_exists(constant($key) . $conf[$key][$i] . ".php")) {
+				require_once constant($key) . $conf[$key][$i] . ".php";
+			}
+		}
+	}
 
 	$app = new App;
